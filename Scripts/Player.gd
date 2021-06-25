fb45b2c2f54700ea3015f9fb985b7e984fc2256b
Chandra Gunawan <56698746+beyondchan28@users.noreply.github.com>
2021-06-25 15:07:18 +0700

    Fixing Tile Set and Adding Scene Transition

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,119 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 60
-const FRICTION = 500
+export var walk_speed = 4.0
+const TILE_SIZE = 16
 
-var velocity = Vector2.ZERO
+signal player_entering_door_signal
+signal player_entered_door_signal
 
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
-onready var animationState = animationTree.get("parameters/playback")
+onready var anim_tree = $AnimationTree
+onready var anim_state = anim_tree.get("parameters/playback")
+onready var ray = $BlockingRayCast2D
+onready var door_ray = $DoorRayCast2D
 
+enum PlayerState {IDLE, TURNING, RUNNING}
+enum FacingDirection {LEFT, RIGHT, UP, DOWN}
+
+var player_state = PlayerState.IDLE
+var facing_direction = FacingDirection.DOWN
+
+var initial_position = Vector2.ZERO
+var input_direction = Vector2.ZERO
+var is_moving = false
+var stop_input: bool = false
+var percent_moved_to_next_tile = 0.0
+
+func _ready():
+	$Sprite.visible = true
+	anim_tree.active = true
+	initial_position = position
+	
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	if player_state == PlayerState.TURNING or stop_input:
+		return
+	elif is_moving == false :
+		process_player_input()
+	elif input_direction != Vector2.ZERO :
+		anim_state.travel("Run")
+		move(delta)
+	else :
+		anim_state.travel("Idle")
+		is_moving = false
 		
-	velocity = move_and_slide(velocity)
+func process_player_input():
+	if input_direction.y == 0 :
+		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+		
+	if input_direction.x == 0 :
+		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+		
+	if input_direction != Vector2.ZERO :
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Run/blend_position", input_direction)
+		anim_tree.set("parameters/Turn/blend_position", input_direction)
+		
+		if need_to_turn() :
+			player_state = PlayerState.TURNING
+			anim_state.travel("Turn")
+		else :
+			initial_position = position
+			is_moving = true
+	else :
+		anim_state.travel("Idle")
+		
+
+func need_to_turn():
+	var new_facing_direction
+	if input_direction.x < 0 :
+		new_facing_direction = FacingDirection.LEFT
+	elif input_direction.x > 0 :
+		new_facing_direction = FacingDirection.RIGHT
+	elif input_direction.y < 0 :
+		new_facing_direction = FacingDirection.UP
+	elif input_direction.y > 0 :
+		new_facing_direction = FacingDirection.DOWN
+		
+	if facing_direction != new_facing_direction:
+		facing_direction = new_facing_direction
+		return true
+	facing_direction = new_facing_direction
+	return false
+
+func finished_turning() :
+	player_state = PlayerState.IDLE
+	
+func entered_door():
+	emit_signal("player_entered_door_signal")
+
+func move(delta):
+	var desired_step : Vector2 = input_direction * TILE_SIZE/4
+	ray.cast_to = desired_step
+	ray.force_raycast_update()
+	
+	door_ray.cast_to = desired_step
+	door_ray.force_raycast_update()
+	
+	if door_ray.is_colliding():
+		if percent_moved_to_next_tile == 0.0 :
+			emit_signal("player_entering_door_signal")
+		percent_moved_to_next_tile += walk_speed * delta
+		if percent_moved_to_next_tile >= 1.0 :
+			position = initial_position + (input_direction * TILE_SIZE)
+			percent_moved_to_next_tile = 0.0
+			is_moving = false
+			stop_input = true
+			$AnimationPlayer.play("Disappear")
+			$Camera2D.clear_current()
+		else :
+			position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
+	
+	elif !ray.is_colliding():
+		percent_moved_to_next_tile += walk_speed * delta
+		if percent_moved_to_next_tile >= 1.0 :
+			position = initial_position + (TILE_SIZE * input_direction)
+			percent_moved_to_next_tile = 0.0
+			is_moving = false
+		else :
+			position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
+	else :
+		is_moving = false
